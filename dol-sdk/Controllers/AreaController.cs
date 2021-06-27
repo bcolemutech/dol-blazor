546d3158d5b2ea8552bcfc63a7d387c6ef4589c1
bcolemutech <bcolemutech@hotmail.com>
2021-06-27 17:52:10 -0500

    Implement area controller methods

--- dol-sdk/Controllers/AreaController.cs
+++ dol-sdk/Controllers/AreaController.cs
@@ -1,5 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using dol_sdk.POCOs;
+using dol_sdk.Services;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace dol_sdk.Controllers
 {
@@ -11,14 +19,48 @@ namespace dol_sdk.Controllers
 
     public class AreaController : IAreaController
     {
-        public async Task EditArea(IArea selectedArea)
+        private const string Bearer = "Bearer";
+        private readonly ISecurityService _security;
+        private readonly HttpClient _client;
+        private readonly string _requestUri;
+        public AreaController(IHttpClientFactory factory, IConfiguration configuration, ISecurityService securityService)
         {
-            throw new System.NotImplementedException();
+            _security = securityService;
+            _client = factory.CreateClient();
+            _requestUri = configuration["DolApiUri"] + "area";
         }
 
-        public Task<IArea> GetArea(int x, int y)
+        public async Task EditArea(IArea selectedArea)
         {
-            throw new System.NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Put, $"{_requestUri}/{selectedArea.X}/{selectedArea.Y}");
+            var IdToken = _security.Identity.FirebaseToken;
+            
+            var contentJson = JsonConvert.SerializeObject(selectedArea);
+
+            request.Headers.Authorization = new AuthenticationHeaderValue(Bearer, IdToken);
+            request.Content = new StringContent(contentJson, Encoding.Default, "application/json");
+            
+            var response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<IArea> GetArea(int x, int y)
+        {
+            var thisUri = new Uri($"{_requestUri}/{x}/{y}");
+            var request = new HttpRequestMessage(HttpMethod.Get, thisUri);
+            var IdToken = _security.Identity.FirebaseToken;
+            
+            request.Headers.Authorization = new AuthenticationHeaderValue(Bearer, IdToken);
+            
+            var response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var stream = await response.Content.ReadAsStreamAsync();
+            
+            var serializer = new JsonSerializer();
+
+            var sr = new StreamReader(stream);
+            var jsonTextReader = new JsonTextReader(sr);
+            return serializer.Deserialize<Area>(jsonTextReader);
         }
     }
 }
