c5dbf8797572529647785c8b0bfabb615f1e76f7
bcolemutech <bcolemutech@hotmail.com>
2021-06-27 15:56:56 -0500

    Area object for validations

---
+++ DolBlazor/Models/Area.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using dol_sdk.Enums;
+using dol_sdk.POCOs;
+
+namespace DolBlazor.Models
+{
+    public class Area: IArea
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        
+        [MaxLength(30)]
+        public string Region { get; set; }
+        
+        [MaxLength(200)]
+        public string Description { get; set; }
+        [Required]
+        public bool IsCoastal { get; set; }
+        
+        [Required]
+        [EnumDataType(typeof(Ecosystem))]
+        public Ecosystem Ecosystem { get; set; }
+        
+        [Required]
+        [EnumDataType(typeof(Navigation))]
+        public Navigation Navigation { get; set; }
+    }
+}
