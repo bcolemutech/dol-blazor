ac745ef6116c9a0795003e98dd3e41085bbecfb3
bcolemutech <bcolemutech@hotmail.com>
2021-07-11 14:13:48 -0500

    Position object for navigation and actions

---
+++ dol-sdk/POCOs/Position.cs
@@ -0,0 +1,22 @@
+﻿using dol_sdk.Enums;
+
+namespace dol_sdk.POCOs
+{
+    public interface IPosition
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        string Populace { get; set; }
+        string Location { get; set; }
+        Action Action { get; set; }
+    }
+
+    public class Position : IPosition
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Populace { get; set; }
+        public string Location { get; set; }
+        public Action Action { get; set; }
+    }
+}
