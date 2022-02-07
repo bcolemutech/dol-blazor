39ae7494bbd031d2047e93e20e44666e63dd3b55
bcolemutech <bcolemutech@hotmail.com>
2022-02-07 00:53:46 -0600

    Issue tracker for Areas

--- DolBlazor/Models/Area.cs
+++ DolBlazor/Models/Area.cs
@@ -1,15 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-using dol_sdk.Enums;
-using dol_sdk.POCOs;
-
-namespace DolBlazor.Models
+﻿namespace DolBlazor.Models
 {
-    public class Area: IArea
+    using System.ComponentModel.DataAnnotations;
+    using dol_sdk.Enums;
+    using dol_sdk.POCOs;
+    using System.Collections.Generic;
+
+    public class Area : IArea
     {
         public Area()
         {
-            
+            Issues = new List<string> { "Area is not setup" };
         }
+
         public Area(IArea area)
         {
             X = area.X;
@@ -19,25 +21,29 @@ namespace DolBlazor.Models
             IsCoastal = area.IsCoastal;
             Ecosystem = area.Ecosystem;
             Navigation = area.Navigation;
+            Issues = new List<string>();
         }
 
         public int X { get; set; }
         public int Y { get; set; }
-        
+
         [StringLength(30)]
         public string Region { get; set; }
-        
+
         [StringLength(200)]
         public string Description { get; set; }
+
         [Required]
         public bool IsCoastal { get; set; }
-        
+
         [Required]
         [EnumDataType(typeof(Ecosystem))]
         public Ecosystem Ecosystem { get; set; }
-        
+
         [Required]
         [EnumDataType(typeof(Navigation))]
         public Navigation Navigation { get; set; }
+
+        public List<string> Issues { get; set; }
     }
 }
