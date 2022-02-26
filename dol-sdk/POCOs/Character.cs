08738a75e770636cf77ffdd787abd670411a3c31
bcolemutech <bcolemutech@hotmail.com>
2022-02-25 21:25:52 -0600

    Moving will be realtime using the Hub not API requests

--- dol-sdk/POCOs/Character.cs
+++ dol-sdk/POCOs/Character.cs
@@ -4,12 +4,10 @@ public interface ICharacter
 {
     string Name { get; set; }
     IPosition Position { get; set; }
-    IPosition Move { get; set; }
 }
 
 public class Character : ICharacter
 {
     public string Name { get; set; }
     public IPosition Position { get; set; }
-    public IPosition Move { get; set; }
 }
