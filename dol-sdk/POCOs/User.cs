48f593c1ce4d50f2eddf3a4334f7d68834c5504b
bcolemutech <bcolemutech@hotmail.com>
2022-03-14 22:43:50 -0500

    Use reference for current user

--- dol-sdk/POCOs/User.cs
+++ dol-sdk/POCOs/User.cs
@@ -5,7 +5,7 @@ using dol_sdk.Enums;
 public interface IUser : IPlayerRequest
 {
     string Password { get; set; }
-    public ICharacter CurrentCharacter { get; set; }
+    public string CurrentCharacter { get; set; }
     public string SessionId { get; set; }
     public string ConnectionId { get; set; }
 }
@@ -14,7 +14,7 @@ public class User : IUser
 {
     public string Email { get; set; }
     public string Password { get; set; }
-    public ICharacter CurrentCharacter { get; set; }
+    public string CurrentCharacter { get; set; }
     public string SessionId { get; set; }
     public string ConnectionId { get; set; }
     public Authority Authority { get; set; }
